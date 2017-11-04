03890416854bf23a38aadfab6ee5fa5b999db552
TonyTheJet <tony@tony-anderson.info>
2017-11-03 18:27:38 -0600

    initial commit

---
+++ header.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Welcome to ProbateEssentials.com!</title>
+		<link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		<link href="<?php bloginfo('template_url'); ?>/css/fonts.min.css" rel="stylesheet" type="text/css" />
+		<link href="<?php bloginfo('template_url'); ?>/style.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body>
+		<div class="container-fluid">
+			body here
+		</div>
