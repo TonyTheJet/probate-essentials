8a60c7a458d480c66ab378753a7d799c181b1523
TonyTheJet <tony@tony-anderson.info>
2017-11-12 20:05:14 -0700

    Great progress on FAQs and videos #time 2h

---
+++ js/main.js
@@ -0,0 +1,7 @@
+jQuery(function(){
+    console.log('document is ready!');
+    jQuery('.accordion').accordion({
+        active: 0,
+        collapsible: true
+    });
+});
---
+++ template-parts/side-banner.php
@@ -0,0 +1,4 @@
+<a class="mini-banner" href="#inheritance-conflicts">INHERITANCE CONFLICTS</a>
+<a class="mini-banner" href="#probating-a-will">PROBATING A WILL</a>
+<a class="mini-banner" href="#estate-administration">ESTATE ADMINISTRATION</a>
+<a class="mini-banner" href="#preventing-probate">PREVENTING PROBATE</a>
